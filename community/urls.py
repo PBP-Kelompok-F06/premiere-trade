7e31b7dc97a0b78d37a3a715cbcadd8e9223a80b
Aryandana Pascua Patiung <nandapascua@Aryandanas-MacBook-Pro.local>
2025-10-22 20:05:05 +0700

    Simpen kerjaan lokal di homepage sebelum narik master

--- community/urls.py
+++ community/urls.py
@@ -1,6 +1,11 @@
+# community/urls.py
+
 from django.urls import path
 from . import views
-        
+
+app_name = 'community'
+
 urlpatterns = [
-    path('', views.index, name='community_home'),
+    path('', views.community_index, name='community_home'),
+    path('reply/<int:post_id>/', views.add_reply, name='add_reply'),
 ]
