36e4cc83bb7815c5d8f5164388f7bfdc6f8c5c98
AdryanRasyad <adryanrasyad@gmail.com>
2025-10-26 18:18:30 +0700

    Tambahin admin

--- rumors/admin.py
+++ rumors/admin.py
@@ -1,3 +1,46 @@
+# rumors/admin.py
 from django.contrib import admin
+from .models import Rumors
 
-# Register your models here.
+@admin.register(Rumors)
+class RumorsAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "pemain",
+        "club_asal",
+        "club_tujuan",
+        "author",
+        "status",
+        "rumors_views",
+        "created_at",
+    )
+    # Field yang bisa diklik untuk lihat detail
+    list_display_links = ("title",)
+
+    list_filter = ("status", "club_asal", "club_tujuan", "author")
+
+    search_fields = (
+        "title",
+        "pemain__nama_pemain",
+        "club_asal__name",
+        "club_tujuan__name",
+        "author__username",
+    )
+    ordering = ("-created_at",)
+
+    #biar gak bisa diubah manual dari admin
+    readonly_fields = (
+        "id",
+        "title",
+        "created_at",
+        "rumors_views",
+    )
+    #tampilan detail
+    fieldsets = (
+        ("Informasi Utama", {
+            "fields": ("author", "pemain", "club_asal", "club_tujuan", "content")
+        }),
+        ("Status & Statistik", {
+            "fields": ("status", "title", "rumors_views", "created_at"),
+        }),
+    )
